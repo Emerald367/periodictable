d7a1fa413312f0206be4166968a80ffd906562af
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-07-23 16:48:21 -0700

    spring boot configured

---
+++ periodic-table/src/test/java/com/github/emerald367/periodic_table/PeriodicTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.emerald367.periodic_table;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeriodicTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
