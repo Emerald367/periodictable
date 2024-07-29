393a45a2840836bbb13c3b02ac81309ded5bb347
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-07-29 15:30:07 -0700

    intro section component connected

--- frontend/periodic-table/src/app/app.component.ts
+++ frontend/periodic-table/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroSection } from './intro-section.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroSection],
   template: `<div class="min-h-screen min-w-full bg-slate-800">
+    <intro-section />
   </div>`,
   styles: []
 })
 export class AppComponent {
-  title = 'Element Explorer';
+
 }
