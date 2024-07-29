3cd7a5e91b6e929a8bc055c866bd70c9451f93be
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-07-29 15:31:08 -0700

    added first part of intro text

---
+++ frontend/periodic-table/src/app/intro-section.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+    selector: 'intro-section',
+    standalone: true,
+    template: 
+    `<div class="text-green-400 text-center text-5xl font-mont pt-2">
+        Welcome to the Element Explorer!
+    </div>`
+    ,
+    styles: []
+})
+export class IntroSection {
+
+}
