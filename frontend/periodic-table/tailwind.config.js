c8d9067264b70e6203c2c1cfdb789705d725f410
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-07-31 15:31:59 -0700

    special classes for tailwind

--- frontend/periodic-table/tailwind.config.js
+++ frontend/periodic-table/tailwind.config.js
@@ -4,7 +4,24 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        '1/18': '5.555555%',
+      },
+      fontFamily: {
+        'merri': ["Merriweather", 'serif'],
+        'mont': ["Montserrat", 'sans-serif']
+      },
+      animation: {
+        fade: 'fadeIn 2s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          from: { opacity: 0},
+          to: { opacity: 1},
+        },
+      },
+    },
   },
   plugins: [],
 }
