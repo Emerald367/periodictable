291ce5bd22439ef0a5bd4189e55d0cf2f94e0db2
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-08-09 18:20:44 -0700

    components for additional element details

---
+++ frontend/periodic-table/src/elementcomponents/helium.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'helium-component',
+    standalone: true,
+    imports: [],
+    template: ``,
+    styles: []
+})
+
+export class HeliumComponent {
+
+}
---
+++ frontend/periodic-table/src/elementcomponents/hydrogen.component.ts
@@ -0,0 +1,13 @@
+import {Component} from '@angular/core'
+
+@Component({
+    selector: 'hydrogen-component',
+    standalone: true,
+    imports: [],
+    template: ``,
+    styles: []
+})
+
+export class HydrogenComponent {
+
+}
