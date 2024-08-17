f87f3cd1f8a1b2696d261ba8d2fe2c8cda862a9d
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-08-16 19:51:30 -0700

    links for hydrogen and helium

--- frontend/periodic-table/src/components/first-row.component.ts
+++ frontend/periodic-table/src/components/first-row.component.ts
@@ -1,18 +1,21 @@
 import {Component} from '@angular/core'
+import {Routes, RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router'
+import {CommonModule} from '@angular/common'
 
 @Component({
     selector: 'first-row',
     standalone: true,
+    imports: [RouterLink, RouterLinkActive, CommonModule, RouterOutlet],
     template: `
      <div class="max-w-full overflow-x-auto mb-1">
       <div class="flex flex-wrap justify-center">
         <!-- Hydrogen -->
-        <div class="element bg-gradient-to-r from-violet-500 to-blue-700 text-center text-white font-mont p-1 hover:border-green-400 border-blue-700 border-2">
+         <div class="element bg-gradient-to-r from-violet-500 to-blue-700 text-center text-white font-mont p-1 hover:border-green-400 border-blue-700 border-2"><a routerLink="/hydrogen" routerLinkActive="active" ariaCurrentWhenActive="page">
           <div class="atomic-number text-xs">1</div>
           <div class="symbol text-sm">H</div>
           <div class="element-name text-xs">Hydrogen</div>
           <div class="atomic-weight text-xs">1.008</div>
-        </div>
+         </a></div>
           <!-- Empty spaces -->
         <div class="empty-space"></div>
         <div class="empty-space"></div>
@@ -37,12 +40,12 @@ import {Component} from '@angular/core'
         <div class="empty-space"></div>
         <div class="empty-space"></div>
         <!-- Helium -->
-        <div class="element bg-gradient-to-r from-lime-500 to-green-700 p-1 text-center text-white font-mont hover:border-green-400 border-green-700 border-2">
+        <div class="element bg-gradient-to-r from-lime-500 to-green-700 p-1 text-center text-white font-mont hover:border-green-400 border-green-700 border-2"><a routerLink="/helium" routerLinkActive="active" ariaCurrentWhenActive="page">
           <div class="atomic-number text-xs">2</div>
           <div class="symbol text-sm">He</div>
           <div class="element-name text-xs">Helium</div>
           <div class="atomic-weight text-xs">4.0026</div>
-        </div>
+        </a></div>
       </div>
     </div>
     `
