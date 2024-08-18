bd41f142a9b371f5a194d19f75b512c11cd06d0d
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-08-17 18:58:23 -0700

    routing configuration

--- frontend/periodic-table/src/app/app.routes.ts
+++ frontend/periodic-table/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HydrogenComponent } from '../elementcomponents/hydrogen.component';
+import { HeliumComponent } from '../elementcomponents/helium.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'hydrogen', component: HydrogenComponent},
+    { path: 'helium', component: HeliumComponent},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', redirectTo: '/home'} //Fallback route
+];
