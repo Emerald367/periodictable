89b232cd037b92e7851f6f31078d514acf08aff9
Emerald367 <142702711+Emerald367@users.noreply.github.com>
2024-08-17 18:57:58 -0700

    config to display element components from routing

--- frontend/periodic-table/src/app/app.component.ts
+++ frontend/periodic-table/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { IntroSection } from '../components/intro-section.component'
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router'
+import { CommonModule} from '@angular/common'
+import { filter } from 'rxjs/operators'
 import { FirstRow } from '../components/first-row.component'
 import { SecondRow } from '../components/second-row.component'
 import { ThirdRow } from '../components/third-row.component'
@@ -14,21 +16,36 @@ import { NinthRow } from '../components/ninth-row.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IntroSection, FirstRow, SecondRow, ThirdRow, FourthRow, FifthRow, SixthRow, SeventhRow, EighthRow, NinthRow],
-  template: `<div class="min-h-screen min-w-full bg-slate-800 text-center overflow-y-auto">
-    <intro-section />
-    <first-row />
-    <second-row />
-    <third-row />
-    <fourth-row />
-    <fifth-row />
-    <sixth-row />
-    <seventh-row />
-    <eighth-row />
-    <ninth-row />
-  </div>`,
+  imports: [RouterOutlet, CommonModule, IntroSection, FirstRow, SecondRow, ThirdRow, FourthRow, FifthRow, SixthRow, SeventhRow, EighthRow, NinthRow],
+  template: `
+    <ng-container *ngIf="isHomePage; else routedContent">
+      <div class="min-h-screen relative min-w-full bg-slate-800 text-center overflow-y-auto">
+       <intro-section />
+       <first-row />
+       <second-row />
+       <third-row />
+       <fourth-row />
+       <fifth-row />
+       <sixth-row />
+       <seventh-row />
+       <eighth-row />
+       <ninth-row />
+      </div>
+     </ng-container>
+     <ng-template #routedContent>
+       <router-outlet></router-outlet>
+     </ng-template>
+     `,
   styles: []
 })
 export class AppComponent {
+  isHomePage = true;
 
+  constructor(private router: Router) {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd)
+    ).subscribe((event: NavigationEnd) => {
+      this.isHomePage = event.url === '/' || event.url === '/home';
+    })
+  }
 }
